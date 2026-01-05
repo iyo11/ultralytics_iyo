37710099d0ac545f30d1884c44e12ebc4c600bce
iyo11 <IYO.MAIL.CN@gmail.com>
2026-01-05 09:20:26 +0800

    + C2LGA

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -73,7 +73,7 @@ from ultralytics.nn.modules.backbone.PoolFormer import PoolFormerBlock
 from ultralytics.nn.modules.backbone.PoolFormerMSConvV2 import PoolFormerMSConvBlockV2
 from ultralytics.nn.modules.backbone.PoolFormerMSConvV3 import PoolFormerMSConvBlockV3
 from ultralytics.nn.modules.backbone.PoolFormerMsConv import PoolFormerMSConvBlock
-from ultralytics.nn.modules.iyo11.LGA_SDPA import LGA_SDPA
+from ultralytics.nn.modules.iyo11.LGA_SDPA import LGA_SDPA, C2LGA
 from ultralytics.nn.modules.iyo11.MultiScaleAdaptiveWindowAttention import MultiScaleAdaptiveWindowAttention
 from ultralytics.nn.modules.v11.ALFS import EFC, MSEF
 from ultralytics.nn.modules.v11.DSSA import C3k2_DSSA, DSSA, C3k_DSSA
@@ -1644,7 +1644,8 @@ def parse_model(d, ch, verbose=True):
             LiteAttention_SDPA,
             StandardAttention_SDPA,
             GatedAttention_SDPA,
-            LGA_SDPA
+            LGA_SDPA,
+            C2LGA
         }
     )
 
@@ -1687,6 +1688,7 @@ def parse_model(d, ch, verbose=True):
             C3k2_SCSA,
             C3k2_GatedAttention,
             C3k2_LiteGatedAttention,
+            C2LGA
         }
     )
 
