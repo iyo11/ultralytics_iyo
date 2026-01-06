e90aa17db9dad9b53e0647973aa0d2bdeec3ab98
iyo11 <iyo.mail.cn@gmail.com>
2026-01-06 18:43:04 +0800

    +ADown

--- train/train_yolov11.py
+++ train/train_yolov11.py
@@ -22,9 +22,9 @@ close_mosaic_count = 30
 
 if __name__ == '__main__':
     #model = YOLO('yolov8n.yaml')
-    #model = YOLO('../models/v11/yolov11n_PoolFormer.yaml')
+    model = YOLO('../models/v11/yolov11n_ADown.yaml')
     #model = YOLO('../models/uf_11/v12/yolov12n_LGSDPA.yaml')
-    model = YOLO('../models/uf_11/v11/yolov11n_QWenGSPDA_moe.yaml')
+    #model = YOLO('../models/uf_11/v11/yolov11n_QWenGSPDA_moe.yaml')
     # 如何切换模型版本, 上面的ymal文件可以改为 yolov8s.yaml就是使用的v8s,
     # 类似某个改进的yaml文件名称为yolov8-XXX.yaml那么如果想使用其它版本就把上面的名称改为yolov8l-XXX.yaml即可（改的是上面YOLO中间的名字不是配置文件的）！
     # model.load('yolov8n.pt') # 是否加载预训练权重,科研不建议大家加载否则很难提升精度
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                 amp=False,  # 如果出现训练损失为Nan可以关闭amp
                 patience=0,
                 project='../runs/train',
-                name='v11n_LGASDPA_NWPU_300',
+                name='v11n_ADown_NWPU_300',
                 #save_period=20,
                 #固定随机种子
                 seed=42
