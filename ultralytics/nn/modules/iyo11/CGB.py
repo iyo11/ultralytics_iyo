f01f99c8f74f96dbffeba6eb46ebe7bbb21dc11e
iyo11 <iyo.mail.cn@gmail.com>
2026-01-17 17:48:21 +0800

    + ContextGuided

--- ultralytics/nn/modules/iyo11/CGB.py
+++ ultralytics/nn/modules/iyo11/CGB.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 
-__all__ = ['ContextGuidedBlock_Down']
+__all__ = ['ContextGuidedBlock_Down_New', 'ContextGuidedBlock_Down']
 
 
-class FGlo(nn.Module):
+class FGlo_New(nn.Module):
     """
     全局上下文模块 (Global Context Refinement)
     利用原生 Conv2d(1x1) 代替 Linear，省去 view/reshape 操作
@@ -23,7 +23,7 @@ class FGlo(nn.Module):
         return x * self.fc(self.avg_pool(x))
 
 
-class ContextGuidedBlock_Down(nn.Module):
+class ContextGuidedBlock_Down_New(nn.Module):
     def __init__(self, nIn, nOut, dilation_rate=2, reduction=16):
         super().__init__()
 
@@ -60,4 +60,246 @@ class ContextGuidedBlock_Down(nn.Module):
         joi_feat = self.reduce(self.act(self.bn(joi_feat)))
         # 全局加权
         output = self.F_glo(joi_feat)
+        return output
+
+class ConvBNPReLU(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1):
+        """
+        args:
+            nIn: number of input channels
+            nOut: number of output channels
+            kSize: kernel size
+            stride: stride rate for down-sampling. Default is 1
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2)
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), bias=False)
+        self.bn = nn.BatchNorm2d(nOut, eps=1e-03)
+        self.act = nn.PReLU(nOut)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        output = self.bn(output)
+        output = self.act(output)
+        return output
+
+
+class BNPReLU(nn.Module):
+    def __init__(self, nOut):
+        """
+        args:
+           nOut: channels of output feature maps
+        """
+        super().__init__()
+        self.bn = nn.BatchNorm2d(nOut, eps=1e-03)
+        self.act = nn.PReLU(nOut)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: normalized and thresholded feature map
+        """
+        output = self.bn(input)
+        output = self.act(output)
+        return output
+
+
+class ConvBN(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1):
+        """
+        args:
+           nIn: number of input channels
+           nOut: number of output channels
+           kSize: kernel size
+           stride: optinal stide for down-sampling
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2)
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), bias=False)
+        self.bn = nn.BatchNorm2d(nOut, eps=1e-03)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        output = self.bn(output)
+        return output
+
+
+class Conv(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1):
+        """
+        args:
+            nIn: number of input channels
+            nOut: number of output channels
+            kSize: kernel size
+            stride: optional stride rate for down-sampling
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2)
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), bias=False)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        return output
+
+
+class ChannelWiseConv(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1):
+        """
+        Args:
+            nIn: number of input channels
+            nOut: number of output channels, default (nIn == nOut)
+            kSize: kernel size
+            stride: optional stride rate for down-sampling
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2)
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), groups=nIn,
+                              bias=False)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        return output
+
+
+class DilatedConv(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1, d=1):
+        """
+        args:
+           nIn: number of input channels
+           nOut: number of output channels
+           kSize: kernel size
+           stride: optional stride rate for down-sampling
+           d: dilation rate
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2) * d
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), bias=False,
+                              dilation=d)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        return output
+
+
+class ChannelWiseDilatedConv(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1, d=1):
+        """
+        args:
+           nIn: number of input channels
+           nOut: number of output channels, default (nIn == nOut)
+           kSize: kernel size
+           stride: optional stride rate for down-sampling
+           d: dilation rate
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2) * d
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), groups=nIn,
+                              bias=False, dilation=d)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        return output
+
+
+class FGlo(nn.Module):
+    """
+    the FGlo class is employed to refine the joint feature of both local feature and surrounding context.
+    """
+
+    def __init__(self, channel, reduction=16):
+        super(FGlo, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+class ContextGuidedBlock_Down(nn.Module):
+    """
+    the size of feature map divided 2, (H,W,C)---->(H/2, W/2, 2C)
+    """
+
+    def __init__(self, nIn, nOut, dilation_rate=2, reduction=16):
+        """
+        args:
+           nIn: the channel of input feature map
+           nOut: the channel of output feature map, and nOut=2*nIn
+        """
+        super().__init__()
+
+        self.conv1x1 = ConvBNPReLU(nIn, nOut, 3, 2)  # size/2, channel: nIn--->nOut
+
+        self.F_loc = ChannelWiseConv(nOut, nOut, 3, 1)
+        self.F_sur = ChannelWiseDilatedConv(nOut, nOut, 3, 1, dilation_rate)
+
+        self.bn = nn.BatchNorm2d(2 * nOut, eps=1e-3)
+        self.act = nn.PReLU(2 * nOut)
+        self.reduce = Conv(2 * nOut, nOut, 1, 1)  # reduce dimension: 2*nOut--->nOut
+
+        self.F_glo = FGlo(nOut, reduction)
+
+    def forward(self, input):
+        output = self.conv1x1(input)
+        loc = self.F_loc(output)
+        sur = self.F_sur(output)
+
+        joi_feat = torch.cat([loc, sur], 1)  # the joint feature
+        joi_feat = self.bn(joi_feat)
+        joi_feat = self.act(joi_feat)
+        joi_feat = self.reduce(joi_feat)  # channel= nOut
+
+        output = self.F_glo(joi_feat)  # F_glo is employed to refine the joint feature
+
         return output
